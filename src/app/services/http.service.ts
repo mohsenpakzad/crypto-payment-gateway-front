17480c0acc9dbbd5f158b8fada11fc8aedd451db
Mohsen Pakzad <mohsen137853@gmail.com>
2022-12-31 23:36:05 +0330

    feat: implement signup and login requests

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,6 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+
+import { LoginUser, SignupUser } from '../models/user';
+import { LocalStorageService } from './local-storage.service';
 
 @Injectable()
 export class HttpService {
-  constructor() {}
+  readonly BASE_URL = 'http://localhost:8080';
+
+  constructor(
+    private http: HttpClient,
+    private localStorageService: LocalStorageService
+  ) {}
+
+  public signup(signupUser: SignupUser) {
+    return this.http
+      .post(`${this.BASE_URL}/signup`, signupUser)
+      .pipe(tap(body => this.localStorageService.setJwt(JSON.stringify(body))));
+  }
+
+  public login(loginUser: LoginUser): Observable<any> {
+    return this.http
+      .post(`${this.BASE_URL}/login`, loginUser)
+      .pipe(tap(body => this.localStorageService.setJwt(JSON.stringify(body))));
+  }
 }
