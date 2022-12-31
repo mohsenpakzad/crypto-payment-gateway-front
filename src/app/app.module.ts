e1cae85c2e511f17d8d6d56c3c21f6a5ab222343
Mohsen Pakzad <mohsen137853@gmail.com>
2022-12-31 22:58:58 +0330

    feat: add needed page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { PaymentComponent } from './pages/payment/payment.component';
+import { UserDashboardComponent } from './pages/user-dashboard/user-dashboard.component';
+import { IndexComponent } from './pages/index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    PaymentComponent,
+    UserDashboardComponent,
+    IndexComponent,
   ],
   imports: [
     BrowserModule,
