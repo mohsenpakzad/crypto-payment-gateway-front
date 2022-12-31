9236c91653d975fe93e4563201cc0dfb517a7cef
Mohsen Pakzad <mohsen137853@gmail.com>
2022-12-31 23:02:27 +0330

    feat: add existing pages to routing module and remove unnecessary codes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
+import { UserDashboardComponent } from './pages/user-dashboard/user-dashboard.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PaymentComponent } from './pages/payment/payment.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { IndexComponent } from './pages/index/index.component';
+
+const routes: Routes = [
+  { path: '', component: IndexComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: UserDashboardComponent },
+  { path: 'payment/:id', component: PaymentComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
