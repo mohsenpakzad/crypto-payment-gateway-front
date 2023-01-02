6d204820db2951c8623d046440ef2252ddf16585
Mohsen Pakzad <mohsen137853@gmail.com>
2023-01-02 20:40:02 +0330

    feat: add user and app assets http requests

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -4,6 +4,16 @@ import { Observable, tap } from 'rxjs';
 
 import { LoginUser, SignupUser } from '../models/user';
 import { LocalStorageService } from './local-storage.service';
+import {
+  CryptoCurrency,
+  FiatBalance,
+  FiatCurrency,
+  Network,
+  Payment,
+  UserTransaction,
+  Wallet,
+  WithdrawalBalance,
+} from '../models/payment';
 
 @Injectable()
 export class HttpService {
@@ -24,5 +34,54 @@ export class HttpService {
     return this.http
       .post(`${this.BASE_URL}/login`, loginUser)
       .pipe(tap(body => this.localStorageService.setJwt(body as string)));
+  }
+
+  public getAllUserPayments(): Observable<Payment[]> {
+    return this.http.get(`${this.BASE_URL}/api/users/payments`) as Observable<
+      Payment[]
+    >;
+  }
+
+  public getAllUserTransactions() {
+    return this.http.get(
+      `${this.BASE_URL}/api/users/transactions`
+    ) as Observable<UserTransaction[]>;
+  }
+
+  public getUserBalance() {
+    return this.http.get(`${this.BASE_URL}/api/users/balance`) as Observable<
+      FiatBalance[]
+    >;
+  }
+
+  public withdrawBalance(withdrawalBalance: WithdrawalBalance) {
+    return this.http.post(
+      `${this.BASE_URL}/api/users/withdraw`,
+      withdrawalBalance
+    );
+  }
+
+  public getAllNetworks() {
+    return this.http.get(`${this.BASE_URL}/api/networks`) as Observable<
+      Network[]
+    >;
+  }
+
+  public getAllCryptoCurrencies() {
+    return this.http.get(
+      `${this.BASE_URL}/api/crypto-currencies`
+    ) as Observable<CryptoCurrency[]>;
+  }
+
+  public getAllWallets() {
+    return this.http.get(`${this.BASE_URL}/api/wallets`) as Observable<
+      Wallet[]
+    >;
+  }
+
+  public getAllFiatCurrencies() {
+    return this.http.get(`${this.BASE_URL}/api/fiat-currencies`) as Observable<
+      FiatCurrency[]
+    >;
   }
 }
