46a520c36ae7101694f65781fd800ca9911522d9
Mohsen Pakzad <mohsen137853@gmail.com>
2023-01-06 03:12:46 +0330

    feat: implement payment component base functionalities

--- src/app/pages/payment/payment.component.ts
+++ src/app/pages/payment/payment.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
-  styleUrls: ['./payment.component.scss']
+  styleUrls: ['./payment.component.scss'],
 })
 export class PaymentComponent {
+  paymentId: number;
+  routeItems: MenuItem[] = [];
 
+  constructor(private route: ActivatedRoute) {
+    this.paymentId = route.snapshot.params['id'];
+    this.routeItems = [
+      { label: 'Choose Crypto', routerLink: 'choose-crypto' },
+      { label: 'Subscribing Transactions ', routerLink: 'sub-transactions' },
+      { label: 'Result', routerLink: 'result' },
+    ];
+  }
 }
